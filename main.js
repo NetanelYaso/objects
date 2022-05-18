2a02ad6d6d2bf932f0e9d5f1141cf2a22a9321e6
NetanelYaso <netanelyaso12@gmail.com>
2022-05-18 17:40:47 +0300

    objects

---
+++ main.js
@@ -0,0 +1,56 @@
+// function myString(mystring){
+//     for (var i = 0; i < mystring.length; i++) {
+//         console.log(mystring[i])
+//     }       
+//     }
+// myString("Netanel Yaso is the KING")
+
+// function changeString(strfromparam){
+//     var newString = "";
+//     for(var i=0 ;i<strfromparam.length ; i++){
+//           newString+= strfromparam[i]+i
+//         }
+//         return newString
+//     }
+// console.log(changeString("hello world"))
+
+// function evevnString(text){
+//     var newstring= "";
+//     for(var i=0;i<text.length;i++){
+//         if(i%2==0){
+//             newstring += "2"
+//         } 
+//         else{
+//         newstring += text[i];
+//         } 
+//     }
+//     return newstring;
+// }
+// console.log(evevnString('hello'));
+
+// function printName(){
+//     var i = 0
+//     var name = prompt
+//     if()
+// }
+// function NameString(){
+// var name=prompt('enter your name')
+// if(name[0]== "A") {
+//     console.log("hello")
+// }
+// else{
+//     console.log("bye")
+// }
+// }
+// NameString()
+
+// function lastNamestring(){
+//     var lastname= prompt()
+//     if(lastname[lastname.length-1]== "E"){
+//         console.log("yes")
+//     }
+//     else{
+//         console.log("no")
+//     }
+// }
+// lastNamestring()
